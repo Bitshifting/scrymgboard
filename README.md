c8d1c1771cf6ffcef3890ae043d96d53fd41cdef
Alex Kersten <alex@dividebyxero.com>
2014-02-15 02:40:15 -0600

    Finish rudimentary API

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ A new dimension in content sharing
 
 ### `/scrymg/story/publish/:type/:title/:content`
 
-Publishes a story, where `type` is one of `{text, image, video, sound}` and `content` is of the respective form `{"blah blah blah", "http://example.com/img.jpg", "http://example.com/video.mp4", "http://example.com/sound.mp3"}`
+Publishes a story, where `type` is one of `{"text", "image", "video", "sound"}` and `content` is of the respective form `{"blah blah blah", "http://example.com/img.jpg", "http://example.com/video.mp4", "http://example.com/sound.mp3"}`
 
 Returns JSON like `{"success":true}`
 
@@ -16,7 +16,7 @@ Returns JSON like `{"success":true}`
 
 Gets all recent stories.
 
-Returns JSON like `[{"_id":"1234567890ab", "time":123456789,"rating":45,"type":"image","title":"test post","content":"http://example.com"},...]`
+Returns JSON like `[{"_id":"1234567890ab", "time":123456789,"user":"Anonymous", "rating":45,"type":"image","title":"test post","content":"http://example.com"},...]`
 
 ### `/scrymg/story/vote/:id/:amount`
 
--- server/scrymg.js
+++ server/scrymg.js
@@ -4,6 +4,7 @@ var app = express();
 var mongo = require('mongodb'), format = require('util').format, ObjectID = require('mongodb').ObjectID;
 var db = new mongo.Db('scrymgdb', new mongo.Server('localhost', 27017, {}), {safe: true});
 
+
 /**
  * Publish story
  */
@@ -14,14 +15,66 @@ app.get('/scrymg/story/publish/:type/:title/:content', function(req, res) {
     db.collection('stories', function(err, collection) {
       if (err) {
         res.end('{"success":false}');
+        db.close();
         throw err;
       }
 
       console.log("Publishing a new story...\n\tTitle: " + req.params.title + "\n\tType: " + req.params.type + "\n\tContent:\n\t" + req.params.content);
-      res.end('{"success":true}');
+
+
+      collection.insert({
+        time: Math.round(new Date().getTime() / 1000),
+        user: "Anonymous",
+        rating: 0,
+        type: req.params.type,
+        title: req.params.title,
+        content: req.params.content
+      }, function(err, count) {
+        if (err) {
+          res.end('{"success":true}');
+          db.close();
+          throw err;
+        }
+
+        res.end('{"success":true}');
+      });
     });
   });
 });
+
+/**
+ * Get stories
+ */
+app.get('/scrymg/story/get/:count', function(req, res) {
+  res.header("Content-Type", "application/json");
+
+  db.open(function() {
+    db.collection('stories', function(err, collection) {
+      if (err) {
+        res.end('[]');
+        db.close();
+        throw err;
+      }
+
+      console.log("Getting the most recent " + req.params.count + " stories...");
+
+      collection.find({}, {}, function (err, cursor) {
+        if (err) {
+          res.end('[]');
+          db.close();
+          throw err;
+        }
+
+        cursor.toArray(function(err, documents) {
+          res.end(JSON.stringify(documents));
+          db.close();
+        });
+      });
+    });
+  });
+});
+
+
 
 app.listen(5555);
 console.log("scrymg server listening on 5555");
