f906441f6ea44ee6b345826a013f836a1d3e9e3c
Alex Kersten <alex@dividebyxero.com>
2014-02-15 16:46:03 -0600

    Basic images are working, need to thumbnail and cache, and work on exploration things

--- server/scrymg.js
+++ server/scrymg.js
@@ -4,44 +4,47 @@ var app = express();
 var mongo = require('mongodb'), format = require('util').format, ObjectID = require('mongodb').ObjectID;
 var db = new mongo.Db('scrymgdb', new mongo.Server('localhost', 27017, {}), {safe: true});
 
+db.open(function() {
+  console.log("Database online");
+});
+
 
 /**
  * Publish story
  */
 app.get('/scrymg/story/publish/:title/:content', function(req, res) {
-//  res.header("Content-Type", "application/json");
 
+  db.collection('stories', function(err, collection) {
+    if (err) {
+      res.jsonp('{"success":false}');
+      throw err;
+    }
 
-  db.open(function() {
-    db.collection('stories', function(err, collection) {
+    console.log("Publishing a new story...\n\tTitle: " + req.params.title + "\n\tType: " + req.params.type + "\n\tContent:\n\t" + req.params.content);
+
+    //Infer the content type, so the correct player can be generated later.
+    var inferredType = "text";
+
+    if (req.params.content.match(/(\.jpg|\.jpeg|\.png|\.gif)$/) != null) {
+      inferredType = "image";
+    }
+
+    console.log("Inferred type:\t" + inferredType);
+
+    collection.insert({
+      time: Math.round(new Date().getTime() / 1000),
+      user: "Anonymous",
+      rating: 0,
+      type: inferredType,
+      title: req.params.title,
+      content: req.params.content
+    }, function(err, count) {
       if (err) {
         res.jsonp('{"success":false}');
-        db.close();
         throw err;
       }
 
-      console.log("Publishing a new story...\n\tTitle: " + req.params.title + "\n\tType: " + req.params.type + "\n\tContent:\n\t" + req.params.content);
-
-      //TODO: Infer the content type, so the correct player can be generated later.
-      var inferredType = "text";
-
-      collection.insert({
-        time: Math.round(new Date().getTime() / 1000),
-        user: "Anonymous",
-        rating: 0,
-        type: inferredType,
-        title: req.params.title,
-        content: req.params.content
-      }, function(err, count) {
-        if (err) {
-          res.jsonp('{"success":true}');
-          db.close();
-          throw err;
-        }
-
-        res.jsonp('{"success":true}');
-        db.close();
-      });
+      res.jsonp('{"success":true}');
     });
   });
 });
@@ -50,37 +53,32 @@ app.get('/scrymg/story/publish/:title/:content', function(req, res) {
  * Get stories
  */
 app.get('/scrymg/story/get/:count/:since', function(req, res) {
-  //res.header("Content-Type", "application/json");
 
-  db.open(function() {
-    db.collection('stories', function(err, collection) {
+  db.collection('stories', function(err, collection) {
+    if (err) {
+      res.jsonp('[]');
+      throw err;
+    }
+
+    console.log("Getting the most recent " + req.params.count + " stories since " + req.params.since + "...");
+
+    collection.find({time : {$gt: parseInt(req.params.since)}}, {}, function (err, cursor) {
       if (err) {
         res.jsonp('[]');
-        db.close();
         throw err;
       }
 
-      console.log("Getting the most recent " + req.params.count + " stories since " + req.params.since + "...");
+      var lowestLimit = Math.min(parseInt(req.params.count), 128);
+      cursor.limit(lowestLimit);
+      cursor.sort({time: -1});
 
-      collection.find({time : {$gt: parseInt(req.params.since)}}, {}, function (err, cursor) {
-        if (err) {
-          res.jsonp('[]');
-          db.close();
-          throw err;
-        }
-
-        var lowestLimit = Math.min(parseInt(req.params.count), 128);
-        cursor.limit(lowestLimit);
-        cursor.sort({time: -1});
-
-        cursor.toArray(function(err, documents) {
-          res.jsonp(documents);
-          db.close();
-        });
+      cursor.toArray(function(err, documents) {
+        res.jsonp(documents);
       });
     });
   });
 });
+
 
 
 
